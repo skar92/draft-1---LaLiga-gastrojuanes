08a34516a738d829c935aa81d8962fc54db5f2fb
skar92 <135007671+skar92@users.noreply.github.com>
2026-08-15 13:18:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -455,8 +455,8 @@ html_pesca_template = """
         let maxLifeTime = 25000 + Math.random() * 15000;
         let randomDepthPct = 0.45 + Math.random() * 0.45;
 
-        let randomVx = getRandomSpeed(2.5, 4);
-        let randomDepthSpeed = getRandomSpeed(0.04, 0.11);
+        let randomVx = getRandomSpeed(2.5, 3);
+        let randomDepthSpeed = getRandomSpeed(0.04, 0.09);
 
         objects.push({
             id: Math.random().toString(36),
