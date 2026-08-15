5ef36ae8f40a07a0bf4630735ebefc55c0cb11ee
skar92 <135007671+skar92@users.noreply.github.com>
2026-08-15 13:16:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -398,7 +398,7 @@ html_pesca_template = """
         return Math.random() < 0.5 ? val : -val;
     }
 
-    let angleSpeed = getRandomSpeed(0.02, 0.025);
+    let angleSpeed = getRandomSpeed(0.01, 0.02);
     let fixedAngle = 0; let chargeForce = 0;
     
     let heli = { x: 100, y: 35, vx: 4, nextChange: 0, radarWidth: 90, active: true, reactiveTime: 0 };
